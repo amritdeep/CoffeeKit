5dfaf14f0d772c8d0715d7622eca14889ac87664
innu thapa <amritenu@innus-MacBook-Pro.local>
2014-07-29 06:20:00 +0545

    Init the project

---
+++ coffeeKit/coffeeKit/DetailViewController.h
@@ -0,0 +1,16 @@
+//
+//  DetailViewController.h
+//  coffeeKit
+//
+//  Created by innu thapa on 7/29/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+@end
---
+++ coffeeKit/coffeeKit/MasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.h
+//  coffeeKit
+//
+//  Created by innu thapa on 7/29/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+@end
